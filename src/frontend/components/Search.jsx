a7e059a1a52e5df5688ce25f7b14dfceb3c7369d
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-13 18:51:32 -0300

    :sparkles: feat: creating search component to search the students

---
+++ src/frontend/components/Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+	return (
+		<form id="formSearchStudent" className="form-search">
+			<input type="text" name="searchInput" id="searchInput" />
+			<button> Pesquisar </button>
+		</form>
+	);
+}
+
+export default Search;
