06d32dc70db9cb358fe4f7c7e4bc81d06b0f01ed
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-13 18:33:58 -0300

    :sparkles: feat: creating component folder and implementing component navbar with navigation list

---
+++ src/frontend/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="main-nav">
+      <header>EdTech</header>
+      <ul className="nav-links">
+        <a className="nav-item" href="/#">
+          <li> Alunos </li>
+        </a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
