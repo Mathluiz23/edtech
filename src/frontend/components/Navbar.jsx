12d57be5501f1f02977a9561613aafcbbd4b40ce
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-13 19:56:35 -0300

    :recycle: feat: Importing css into component

--- src/frontend/components/Navbar.jsx
+++ src/frontend/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import "../css/navbar.css";
 
 function Navbar() {
-  return (
-    <nav className="main-nav">
-      <header>EdTech</header>
-      <ul className="nav-links">
-        <a className="nav-item" href="/#">
-          <li> Alunos </li>
-        </a>
-      </ul>
-    </nav>
-  );
-};
+	return (
+		<nav className="main-nav">
+			<header>EdTech</header>
+			<ul className="nav-links">
+				<a className="nav-item" href="/#">
+					<li> Alunos </li>
+				</a>
+			</ul>
+		</nav>
+	);
+}
 
 export default Navbar;
