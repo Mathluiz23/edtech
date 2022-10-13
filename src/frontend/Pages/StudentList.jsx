15b21d9c3d0b001bda636faf3a3201cbb60b9cd8
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-13 19:58:30 -0300

    :recycle: feat: Importing header component and add new div for implement style

--- src/frontend/Pages/StudentList.jsx
+++ src/frontend/Pages/StudentList.jsx
@@ -1,44 +1,49 @@
 import { React } from "react";
+import Header from "../components/Header";
 import Search from "../components/Search";
+import "../css/studentList.css";
 
 function StudentList() {
 	const studentsList = ["2", "3"];
 	return (
-		<div>
-			<div className="top-actions">
-				<Search />
-				<a className="btn btn-dark" href="studentManager.html">
-					Cadastrar aluno
-				</a>
+		<section className="container">
+			<Header />
+			<div className="content-page">
+				<div className="top-actions">
+					<Search />
+					<a className="btn btn-dark" href="studentManager.html">
+						Cadastrar aluno
+					</a>
+				</div>
+				<table id="studentList" className="table-list">
+					<thead>
+						<tr>
+							<th>Registro Acadêmico</th>
+							<th>Nome </th>
+							<th>CPF</th>
+							<th>Ações</th>
+						</tr>
+					</thead>
+					<tbody>
+						{studentsList.map((student) => {
+							return (
+								<tr key={student}>
+									<td>{"2"} </td>
+									<td>{"3"} </td>
+									<td>{"4"} </td>
+									<td>
+										<a>Editar</a>
+										<a className="removeStudent" href="/#">
+											Excluir
+										</a>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
 			</div>
-			<table id="studentList" className="table-list">
-				<thead>
-					<tr>
-						<th>Registro Acadêmico</th>
-						<th>Nome </th>
-						<th>CPF</th>
-						<th>Ações</th>
-					</tr>
-				</thead>
-				<tbody>
-					{studentsList.map((student) => {
-						return (
-							<tr key={student}>
-								<td>{"2"} </td>
-								<td>{"3"} </td>
-								<td>{"4"} </td>
-								<td>
-									<a>Editar</a>
-									<a className="removeStudent" href="/#">
-										Excluir
-									</a>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
+		</section>
 	);
 }
 
