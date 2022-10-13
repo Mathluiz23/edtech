ffec9df038b5b26e82fa5d2062e6cb35c796ba98
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2022-10-13 19:11:41 -0300

    :recycle: feat: modifying studentlist to functional component and no longer a class component

--- src/frontend/Pages/StudentList.jsx
+++ src/frontend/Pages/StudentList.jsx
@@ -1,50 +1,45 @@
 import { React } from "react";
+import Search from "../components/Search";
 
-export default class StudentListPage extends React {
-	constructor(props) {
-		super(props);
-		this.state = {
-			studentsList: [],
-		};
-	}
-
-	render() {
-		return (
-			<div>
-				<div className="top-actions">
-					{/* Component Search here */}
-					<a className="btn btn-dark" href="studentManager.html">
-						Cadastrar aluno
-					</a>
-				</div>
-				<table id="studentList" className="table-list">
-					<thead>
-						<tr>
-							<th>Registro Acadêmico</th>
-							<th>Nome </th>
-							<th>CPF</th>
-							<th>Ações</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.studentsList.map((student) => {
-							return (
-								<tr key={student.ra}>
-									<td>{student.ra} </td>
-									<td>{student.nome} </td>
-									<td>{student.cpf} </td>
-									<td>
-										<a>Editar</a>
-										<a className="removeStudent" href="/#">
-											Excluir
-										</a>
-									</td>
-								</tr>
-							);
-						})}
-					</tbody>
-				</table>
+function StudentList() {
+	const studentsList = ["2", "3"];
+	return (
+		<div>
+			<div className="top-actions">
+				<Search />
+				<a className="btn btn-dark" href="studentManager.html">
+					Cadastrar aluno
+				</a>
 			</div>
-		);
-	}
+			<table id="studentList" className="table-list">
+				<thead>
+					<tr>
+						<th>Registro Acadêmico</th>
+						<th>Nome </th>
+						<th>CPF</th>
+						<th>Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+					{studentsList.map((student) => {
+						return (
+							<tr key={student}>
+								<td>{"2"} </td>
+								<td>{"3"} </td>
+								<td>{"4"} </td>
+								<td>
+									<a>Editar</a>
+									<a className="removeStudent" href="/#">
+										Excluir
+									</a>
+								</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
+
+export default StudentList;
